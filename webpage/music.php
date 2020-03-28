4a0f3af70adcc9f034452a25f62849ebc87c4fc1
Akbarjohn1999 <Lycaanthrope@gmail.com>
2020-03-27 17:06:26 -0700

    Finished

--- webpage/music.php
+++ webpage/music.php
@@ -1,8 +1,7 @@
 <?php 
+	$playlist = (isset($_REQUEST["playlist"]))?$_REQUEST["playlist"]:NULL;
+	$shuffle = (isset($_REQUEST["shuffle"]))?$_REQUEST["shuffle"]:NULL;
 
-$playlist = (isset($_REQUEST["playlist"]))?$_REQUEST["playlist"]:NULL;
-
-$shuffle = (isset($_REQUEST["shuffle"]))?$_REQUEST["shuffle"]:NULL;
 
 function songSize($fileSize) {
   if($fileSize >= 0 && $fileSize < 1024){
@@ -15,70 +14,79 @@ function songSize($fileSize) {
     return round($fileSize/1048576, 2) . " mb";
   }
 }
-
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
- "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title>Music Viewer</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <link href="viewer.css" type="text/css" rel="stylesheet" />
-  </head>
-  <body>
-    <div id="header">
-
-      <h1>190M Music Playlist Viewer</h1>
-      <h2>Search Through Your Playlists and Music</h2>
-    </div>
+ ?>
 
 
-    <div id="listarea">
-      <ul id="musiclist">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Music Viewer</title>
+	<link rel="stylesheet" type="text/css" href="viewer.css" />
+</head>
+<body>
+	<div id="header">
+		<h1>190M Music Playlist Viewer</h1>
+    	<h2>Search Through Your Playlists and Music</h2>
+		<p id="topClick"><a href="music.php?shuffle=on">Shuffle</a></p>
+	</div>
+		
 
-        <?php
-        if($playlist)
-        {
-          $songs = file("songs/$playlist", FILE_IGNORE_NEW_LINES);
-        }
-        elseif($shuffle)
-        {
-          $songs = glob("songs/*.mp3");
-          shuffle($songs);
-        }
-        else
-        {
-          $songs = glob("songs/*.mp3");
-        }
+	<div id="listarea">
+		<ul id="musiclist">
+			<?php 
+				if ($playlist)
+				{
+					$songs = file("songs/$playlist",FILE_IGNORE_NEW_LINES);					
+				}
+				elseif($shuffle)
+		        {
+		          $songs = glob("songs/*.mp3");
+		          shuffle($songs);
+		        }
+				else
+				{
+					$songs = glob("songs/*.mp3");
+				}
+				foreach ($songs as $song) 
+				{
+					if (strstr($song, ".mp3")) 
+					{
+				
+				
+			?>		
+					<li class="mp3item" >
+	             		<a href="<?= $song ?> "> 
+	             			<?= basename($song) ?>	              	
+	              		</a>
+	              		(<?= songSize(filesize("songs/".basename($song))) ?>)
+	            	</li>
+            <?php
+            		}		
+				}
+			 
+			 	$set= glob("songs/*.m2u");
+			 	$set= glob("songs/*.txt");
+			 	if(!($playlist))
+			 	{
+				 	foreach ($set as $song)
+				 	{ 
+			 	?>
+				 	 <li class="playlistitem" >
+             			 <a href="music.php?playlist=<?= basename($song)?>"> 
+             			 	<?= basename($song) ?>   
+             			</a>
+						(<?= songSize(filesize("songs/". basename($song))) ?>)
+            		</li>
 
-        foreach ( $songs as $song) 
-        {
-          if(strstr($song, ".mp3"))
-          {
-          ?>
-            <li class="mp3item" >
-              <a href="<?= $item?> "> <?= basename($song) ?></a> (<?= songSize(filesize("songs/". basename($song))) ?>)
-            </li>
-            <?php 
-          } 
-        }
 
-        $set = glob("songs/*.m3u");
-        if(!($playlist))
-        {
-          foreach ( $set as $song) 
-          {
-          ?>
-            <li class="playlistitem" >
-              <a href="music.php?playlist=<?= basename($song)?>"> <?= basename($song) ?>   </a>
-            </li>
+			 	<?php 
+			 		}
+			 		} 
+			 	?>			
+		</ul>
+	</div>
+		
 
-            <?php 
-          } 
-        }   ?>
-
-      </ul>
-    </div>
-  </body>
+	
+</body>
 </html>
